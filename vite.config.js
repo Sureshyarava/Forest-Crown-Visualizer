559449e4e2b987f3bf92526a85609b1ed352da1c
Venkata Suresh <user@USERs-iMac.local>
2025-02-13 15:41:28 -0500

    added code to display the geo spatial images

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '/Forest-Crown-Visualizer/',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    },
+
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+    }
+  };
+});
